b138b6ec20a11a16b0927b1e3c0ed92dd50ed8ea
Akash Bharadwaj Karthik <bharadwajkarthik.a@northeastern.edu>
2023-10-03 12:37:14 -0400

    changing routes and getAll controller logic

--- routes/assignment-route.js
+++ routes/assignment-route.js
@@ -6,32 +6,28 @@ const validateAssignmentSchema = require('../schema/validate-assignment-schema')
 const assignmentRouter = express.Router();
 
 assignmentRouter.route('/v1/assignments')
-      .all(MiddlewareAPI.onlyAllowMethods(['GET', 'POST']))
-      .post(MiddlewareAPI.invalidateReqWithQueryParams,
-            MiddlewareAPI.invalidateNonJSONReqPayload,
-            MiddlewareAPI.validateDTO(validateAssignmentSchema),
+      .all(MiddlewareAPI.onlyAllowMethods(['GET', 'POST']),
+            MiddlewareAPI.invalidateReqWithQueryParams,
+            MiddlewareAPI.invalidateNonJSONReqPayload,)
+      .post(MiddlewareAPI.validateDTO(validateAssignmentSchema),
             MiddlewareAPI.tokenBasedAuthentication.BASIC,
             AssignmentController.createAssignment)
-      .get(MiddlewareAPI.invalidateNonJSONReqPayload,
-           MiddlewareAPI.invalidateNonEmptyReqBody,
+      .get(MiddlewareAPI.invalidateNonEmptyReqBody,
            MiddlewareAPI.tokenBasedAuthentication.BASIC,
            AssignmentController.getAllAssignmentsByUserId)
 
 assignmentRouter.route('/v1/assignments/:assignmentId')
-      .all(MiddlewareAPI.onlyAllowMethods(['PUT', 'DELETE', 'GET']))
-      .get(MiddlewareAPI.invalidateReqWithQueryParams,
-           MiddlewareAPI.invalidateNonJSONReqPayload,
-           MiddlewareAPI.invalidateNonEmptyReqBody,
+      .all(MiddlewareAPI.onlyAllowMethods(['PUT', 'DELETE', 'GET']),
+           MiddlewareAPI.invalidateReqWithQueryParams,
+           MiddlewareAPI.invalidateNonJSONReqPayload,)
+      .get(MiddlewareAPI.invalidateNonEmptyReqBody,
            MiddlewareAPI.tokenBasedAuthentication.BASIC,
            AssignmentController.getAssignmentById)
-      .put(MiddlewareAPI.invalidateReqWithQueryParams,
-           MiddlewareAPI.invalidateNonJSONReqPayload,
-           MiddlewareAPI.validateDTO(validateAssignmentSchema),
+      .put(MiddlewareAPI.validateDTO(validateAssignmentSchema),
            MiddlewareAPI.tokenBasedAuthentication.BASIC,
            AssignmentController.putAssignmentById)
-      .delete(MiddlewareAPI.invalidateReqWithQueryParams,
-            MiddlewareAPI.invalidateNonJSONReqPayload,
-            MiddlewareAPI.invalidateNonEmptyReqBody,
+      .delete(MiddlewareAPI.invalidateNonEmptyReqBody,
             MiddlewareAPI.tokenBasedAuthentication.BASIC,
             AssignmentController.deleteAssignmentById)
+
 module.exports = assignmentRouter;
