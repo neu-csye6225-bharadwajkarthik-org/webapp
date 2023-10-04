b4056d3d4d3b23e928d9c60a0253b2d1541da094
Akash Bharadwaj Karthik <bharadwajkarthik.a@northeastern.edu>
2023-10-04 14:01:49 -0400

    Revert "draft1"
    This reverts commit d825b9b07b306f4a1426aabceb5d10f72d4c6b31.

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const userModel = require('./user-model')
-const assignmentModel = require('./assignment-model')
-const userAssignmentModel = require('./user-assignment-model')
-
-module.exports = {
-   userModel,
-   assignmentModel,
-   userAssignmentModel
-}
--- schema/validate-assignment-schema.js
+++
@@ -1,4 +0,0 @@
-const ajvInstance = require('./ajv-instance.js');
-const assignmentSchema = require('./assignment-schema.json');
-
-module.exports = ajvInstance.compile(assignmentSchema)
