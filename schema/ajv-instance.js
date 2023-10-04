143a58521ec2761911d104bc880cb0930838f339
Akash Bharadwaj Karthik <bharadwajkarthik.a@northeastern.edu>
2023-10-04 14:01:49 -0400

    Revert "draft1"
    This reverts commit d825b9b07b306f4a1426aabceb5d10f72d4c6b31.

--- schema/ajv-instance.js
+++
@@ -1,7 +0,0 @@
-const Ajv = require('ajv')
-const addFormats = require('ajv-formats')
-
-const ajvInstance = new Ajv({allErrors : true, removeAdditional: true })
-addFormats(ajvInstance)
-
-module.exports = ajvInstance;
