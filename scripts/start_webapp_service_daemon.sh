37cf0aa887530831ec654a9e9177bfff5a2c35e4
Akash Bharadwaj Karthik <bharadwajkarthik.a@northeastern.edu>
2023-11-05 20:33:13 -0500

    moving .env construction in wf rather than pulumi

--- scripts/start_webapp_service_daemon.sh
+++ scripts/start_webapp_service_daemon.sh
@@ -3,7 +3,9 @@
 # move the systemd service from /tmp/webapp.service to /etc/systemd/system/webapp.service using sudo
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
 
+# Set ownership (chown) to csye6225:csye6225
+sudo chown csye6225:csye6225 /etc/systemd/system/webapp.service
+
 # reload, enable and start the webapp.service daemon
 sudo systemctl daemon-reload
-sudo systemctl start webapp.service
 sudo systemctl enable webapp.service
